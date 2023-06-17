32ed10fe35bf714a360b0993038a1f7e45f4940d
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-06-17 13:30:48 +0300

    DONE: hw03-mongodb

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,22 +1,16 @@
-const {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} = require("../models/contacts");
+const { Contact } = require("../models/contactSchema");
 
 const { HttpError, cntrlWrapper } = require("../helpers/index");
 
 const getAll = async (req, res) => {
-  const result = await listContacts();
+  const result = await Contact.find();
 
   res.status(200).json(result);
 };
 
 const getById = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await getContactById(contactId);
+  const result = await Contact.findById(contactId);
 
   if (!result) {
     throw HttpError(404, "Not found");
@@ -26,8 +20,7 @@ const getById = async (req, res, next) => {
 };
 
 const add = async (req, res, next) => {
-  const { name, email, phone } = req.body;
-  const results = await addContact(name, email, phone);
+  const results = await Contact.create(req.body);
 
   res.status(201).json(results);
 };
@@ -36,7 +29,23 @@ const updateById = async (req, res, next) => {
   const body = req.body;
   const { contactId } = req.params;
 
-  const result = await updateContact(contactId, body);
+  const result = await Contact.findByIdAndUpdate(contactId, body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+
+const updateFavorite = async (req, res, next) => {
+  const body = req.body;
+  const { contactId } = req.params;
+
+  const result = await Contact.findByIdAndUpdate(contactId, body, {
+    new: true,
+  });
   if (!result) {
     throw HttpError(404, "Not found");
   }
@@ -46,13 +55,11 @@ const updateById = async (req, res, next) => {
 
 const deleteById = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await getContactById(contactId);
+  const result = await Contact.findByIdAndDelete(contactId);
 
   if (!result) {
     throw HttpError(404, "Not found");
   }
-
-  await removeContact(contactId);
 
   res.status(200).json({
     message: "contact deleted",
@@ -64,5 +71,6 @@ module.exports = {
   getById: cntrlWrapper(getById),
   add: cntrlWrapper(add),
   updateById: cntrlWrapper(updateById),
+  updateFavorite: cntrlWrapper(updateFavorite),
   deleteById: cntrlWrapper(deleteById),
 };
---
+++ models/contactSchema.js
@@ -0,0 +1,66 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+// const { handleMongooseError } = require("../helpers/index");
+
+const dateRegexp = /^\(\d{3}\) \d{3}-\d{4}$/;
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      match: dateRegexp,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+// contactSchema.post("save", handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string()
+    .min(2)
+    .max(15)
+    .messages({
+      "any.required": `missing required name field`,
+    })
+    .required(),
+  email: Joi.string()
+    .email()
+    .messages({
+      "any.required": `missing required email field`,
+    })
+    .required(),
+  phone: Joi.string()
+    .min(5)
+    .max(15)
+    .regex(dateRegexp)
+    .messages({
+      "any.required": `missing required phone field`,
+    })
+    .required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
+
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact, schemas };
