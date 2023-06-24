d8410ff80f85010dae7583b9d358c21336786200
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-06-24 15:48:30 +0300

    done auth

--- middlewars/index.js
+++ middlewars/index.js
@@ -1,9 +1,11 @@
 const { validateBody } = require("./validateBody");
 const { validateFavorite } = require("./validateBody");
 const isValidId = require("./isValidId");
+const authenticate = require("./authenticate");
 
 module.exports = {
   validateBody,
   isValidId,
   validateFavorite,
+  authenticate,
 };
